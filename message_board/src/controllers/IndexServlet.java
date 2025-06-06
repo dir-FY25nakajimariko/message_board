8fcd2d370de3055124fefcafd67c2cc95560672a
dir-FY25nakajimariko <riko.nakajima.a212@dirbato.co.jp>
2025-06-06 11:26:22 +0900

    getAllMessagesを追加

--- message_board/src/controllers/IndexServlet.java
+++ message_board/src/controllers/IndexServlet.java
@@ -2,9 +2,13 @@ package controllers;
 import java.io.IOException; 
 import javax.servlet.ServletException; 
 import javax.servlet.annotation.WebServlet;
- import javax.servlet.http.HttpServlet; 
+import javax.servlet.http.HttpServlet; 
 import javax.servlet.http.HttpServletRequest; 
 import javax.servlet.http.HttpServletResponse; 
+import java.util.List; 
+import javax.persistence.EntityManager; 
+import models.Message; 
+import utils.DBUtil;
 /** 
 * Servlet implementation class IndexServlet 
 */
@@ -21,8 +25,10 @@ super();
 /** 
 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response) 
 */
- protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { 
-// TODO Auto-generated method stub
- response.getWriter().append("Served at: ").append(request.getContextPath()); 
-} 
+ protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{ 
+EntityManager em = DBUtil.createEntityManager(); 
+List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList(); 
+response.getWriter().append(Integer.valueOf(messages.size()).toString()); 
+em.close(); 
+}
 }
