604fd9b3fe1bac7ce86182b98d6fffa1e4c3d030
dir-FY25nakajimariko <riko.nakajima.a212@dirbato.co.jp>
2025-06-06 16:32:10 +0900

    ページネーションを追加

--- message_board/src/models/Message.java
+++ message_board/src/models/Message.java
@@ -9,12 +9,16 @@ import javax.persistence.Table;
 import javax.persistence.NamedQueries; 
 import javax.persistence.NamedQuery;
 @Entity
-@NamedQueries({ 
-@NamedQuery( 
-name = "getAllMessages", 
-query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
- ) 
-}) 
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllMessages",
+            query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+            ),
+    @NamedQuery(
+            name = "getMessagesCount",
+            query = "SELECT COUNT(m) FROM Message AS m"
+            )
+ })
  @Table(name = "messages") 
 public class Message { 
 @Id
